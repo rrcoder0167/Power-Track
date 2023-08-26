5ee275ad65ae7f4aab3b46b5a266fc86528835fa
rrcoder0167 <riddhiman.rana@gmail.com>
2023-08-25 20:24:26 -0700

    revert port change back to 8000

--- manage.py
+++ manage.py
@@ -9,14 +9,12 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'power_track_project.settings')
     try:
         from django.core.management import execute_from_command_line
-        from django.core.management.commands.runserver import Command as runserver
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    runserver.default_port = "3000"
     execute_from_command_line(sys.argv)
 
 
